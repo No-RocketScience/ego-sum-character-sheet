c0b3430c165c3cf5f86d9042da646bcfa6054edc
NoRocketScience <florian.rauscher@eclipso.at>
2024-10-20 23:15:09 +0200

    added gridstack and statcounter design

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,10 +1,18 @@
 <script setup>
-import { ref } from 'vue'
+import 'gridstack/dist/gridstack.min.css';
+import { onMounted, ref } from 'vue'
+import { GridStack } from 'gridstack';
 import LabelText from '../LabelText/LabelText.vue';
-import AdvancementPointsCounter from '../AdvancementPointsCounter/AdvancementPointsCounter.vue';
+import AdvancementPointsCounter from './AdvancementPointsCounter/AdvancementPointsCounter.vue';
+import StatDisplay from './StatDisplay/StatDisplay.vue';
 
 const nameInput = ref('Name')
 const aspectInput = ref('Aspect')
+
+const stats = ['STR', 'STA', 'AGI', 'DEX', 'CHA', 'WIL', 'ANI', 'SMA', 'DUR', 'RES', 'PER', 'ABI']
+
+onMounted(() => { GridStack.init({ disableResize: true, itemClass: 'stat-display-container', margin: 0 }); });
+
 </script>
 
 <template>
@@ -12,5 +20,11 @@ const aspectInput = ref('Aspect')
         <LabelText :name="nameInput" />
         <AdvancementPointsCounter />
         <LabelText :name="aspectInput" />
+        <div class="w-100" />
+        <div class="col-12">
+            <div class="row grid-stack">
+                <StatDisplay v-for="stat in stats" :statName="stat" :id="stats.indexOf(stat)" />
+            </div>
+        </div>
     </div>
 </template>
---
+++ src/components/Header/StatDisplay/StatDisplay.vue
@@ -0,0 +1,54 @@
+<script setup>
+import { ref } from 'vue';
+
+const props = defineProps({ statName: String, id: Number });
+
+const number = ref(null);
+
+</script>
+<template>
+    <div class="stat-display-container">
+        <span class="stat-info stat-name">{{ statName || 'StatName' }}</span>
+        <span class="stat-info stat-number" ref="number">1</span>
+    </div>
+</template>
+
+<style lang="css" scoped>
+.stat-display-container {
+    --width: 80px;
+    width: var(--width);
+    height: calc(var(--width) + (var(--width) * (5/16)));
+    margin: .25em;
+}
+
+.stat-info {
+    border: 3px solid #1a1a1a;
+    flex: 0 0 auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.stat-name {
+    border-radius: 5px;
+    padding: 0 .25em;
+    background-color: white;
+    font-size: 25px;
+    --width-reduction: 20px;
+    width: calc(var(--width) - var(--width-reduction));
+    margin-left: calc(var(--width-reduction) / 2);
+    margin-right: calc(var(--width-reduction) / 2);
+}
+
+.stat-number {
+    font-size: calc(var(--width) / 2);
+    font-weight: bold;
+    border-radius: 100%;
+    padding: .75em;
+    position: relative;
+    top: calc(var(--width) * (-5/16));
+    z-index: -1;
+    height: var(--width);
+    width: var(--width);
+}
+</style>
